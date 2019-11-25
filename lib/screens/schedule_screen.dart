510e60f601e48ef8aeeedb4af0daa675221058e4
Kacper Procner <procner.kacper@gmail.com>
2019-11-25 12:09:48 +0100

    switching the day with swipe

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -1,4 +1,9 @@
+import 'package:all_in_one/models/days.dart';
+import 'package:all_in_one/providers/schedule.dart';
+import 'package:all_in_one/widgets/day_switcher.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/app_drawer.dart';
 import 'add_schedule_day_screen.dart';
@@ -9,6 +14,29 @@ class ScheduleScreen extends StatefulWidget {
 }
 
 class _ScheduleScreenState extends State<ScheduleScreen> {
+  static final String today = DateFormat.EEEE().format(DateTime.now());
+  String _pickedDay = Day.containsKey(today) ? today : 'Monday';
+  bool isLoading = true;
+  String direction;
+
+  void _changeDay(String direction) {
+    if (direction == 'forward') {
+      int dayIndex = Days.indexOf(_pickedDay) + 1;
+      if (dayIndex > 4) {
+        dayIndex = 0;
+      }
+      setState(() {
+        _pickedDay = Days[dayIndex];
+      });
+    } else {
+      int dayIndex = Days.indexOf(_pickedDay) - 1;
+      if (dayIndex < 0) {
+        dayIndex = 4;
+      }
+      setState(() => _pickedDay = Days[dayIndex]);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +49,63 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
         onPressed: () =>
             Navigator.of(context).pushNamed(AddScheduleDayScreen.routeName),
       ),
-      body: Center(child: Text('Plan Lekcji')),
+      body: FutureBuilder(
+          future: Provider.of<Schedule>(context, listen: false).fetchSchedule(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              print(dataSnapshot.connectionState);
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (dataSnapshot.error != null) {
+                return Center(
+                  child: Text('An error occurred!'),
+                );
+              } else {
+                return GestureDetector(
+                  behavior: HitTestBehavior.translucent,
+                  onPanEnd: (details) {
+                    direction = '';
+                  },
+                  onPanUpdate: (details) {
+                    if (details.delta.dx < 0 && direction != 'forward') {
+                      direction = 'forward';
+                      _changeDay('forward');
+                    }
+                    if (details.delta.dx > 0 && direction != 'back') {
+                      direction = 'back';
+                      _changeDay('back');
+                    }
+                  },
+                  child: Consumer<Schedule>(
+                    builder: (ctx, schedule, child) => Column(
+                      children: [
+                        child,
+                        Expanded(
+                          child: SingleChildScrollView(
+                            child: AnimatedSwitcher(
+                              transitionBuilder:
+                                  (Widget child, Animation<double> animation) {
+                                return ScaleTransition(
+                                    child: child, scale: animation);
+                              },
+                              duration: const Duration(milliseconds: 200),
+                              child: Center(child: Text('Plan Lekcji')),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    child: DaySwitcher(
+                      changeDay: _changeDay,
+                      day: _pickedDay,
+                    ),
+                  ),
+                );
+              }
+            }
+          }),
     );
   }
 }
