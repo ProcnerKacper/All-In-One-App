8fdf3e4df74e5f5d847e5ef5a9a37e9af15689d9
Kacper Procner <procner.kacper@gmail.com>
2019-11-25 12:05:40 +0100

    create form to add day

--- lib/screens/add_schedule_day_screen.dart
+++ lib/screens/add_schedule_day_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../models/days.dart';
 import '../models/subject.dart';
 import '../providers/schedule.dart';
+import '../widgets/add_subject.dart';
 
 class AddScheduleDayScreen extends StatefulWidget {
   static const routeName = '/add-schedule-day';
@@ -13,6 +14,53 @@ class AddScheduleDayScreen extends StatefulWidget {
 
 class _AddScheduleDayScreenState extends State<AddScheduleDayScreen> {
   final _formKey = GlobalKey<FormState>();
+  String _pickedDay;
+  Map<String, dynamic> _formData = {};
+  List<AddSubject> _listOfTextForm = [];
+
+  @override
+  void initState() {
+    Subject sub = Subject();
+    _listOfTextForm.add(
+      AddSubject(
+        subject: sub,
+        index: 0,
+        key: UniqueKey(),
+      ),
+    );
+    super.initState();
+  }
+
+  void _addSubject() {
+    int index = _listOfTextForm.length;
+    Subject sub = Subject();
+    setState(() {
+      _listOfTextForm.add(AddSubject(
+        subject: sub,
+        index: index,
+        remove: _removeSubject,
+        key: UniqueKey(),
+      ));
+    });
+  }
+
+  void _removeSubject(int index) {
+    setState(() {
+      _listOfTextForm.removeWhere((i) => i.index == index);
+    });
+  }
+
+  void _onSave() {
+    bool allValid = true;
+    _listOfTextForm.forEach((form) => allValid = form.isValid());
+    if (!_formKey.currentState.validate()) allValid = false;
+    if (allValid) {
+      _formKey.currentState.save();
+      List<Subject> subjects = _listOfTextForm.map((it) => it.subject).toList();
+      Provider.of<Schedule>(context).addDay(subjects, _pickedDay);
+      Navigator.of(context).pop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +76,38 @@ class _AddScheduleDayScreenState extends State<AddScheduleDayScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                DropdownButtonFormField(
+                  hint: const Text('Wybierz dzień'),
+                  value: _pickedDay,
+                  items: Day.keys
+                      .map((day) => DropdownMenuItem(
+                            value: day,
+                            child: Text(
+                              Day[day],
+                            ),
+                          ))
+                      .toList(),
+                  onChanged: (day) {
+                    if (day != null) {
+                      setState(() => _pickedDay = day);
+                    }
+                  },
+                  validator: (String val) =>
+                      val == null ? 'Nie wybrano dnia!' : null,
+                  onSaved: (value) => setState(() => _formData['day'] = value),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ..._listOfTextForm,
+                FlatButton.icon(
+                  textColor: Theme.of(context).primaryColor,
+                  icon: const Icon(Icons.add),
+                  label: const Text('Dodaj Lekcje'),
+                  onPressed: _addSubject,
+                ),
                 RaisedButton(
-                  onPressed: () {},
+                  onPressed: _onSave,
                   child: const Text('Dodaj dzień'),
                   color: Theme.of(context).accentColor,
                   textColor: Colors.white,
