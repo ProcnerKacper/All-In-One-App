90505bc19b97e118a97aa466255b0a504700b345
Kacper Procner <k.procner@xberry.tech>
2020-03-09 19:18:11 +0100

    remove divider from drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -12,7 +12,6 @@ class AppDrawer extends StatelessWidget {
             title: const Text('Menu'),
             automaticallyImplyLeading: false,
           ),
-          Divider(),
           ListTile(
             leading: const Icon(Icons.school),
             title: const Text('Plan Lekcji'),
